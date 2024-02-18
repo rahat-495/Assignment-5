584ba30ae69b55750c04efc5d678e8798c0db1b5
rahat-495 <147464855+rahat-495@users.noreply.github.com>
2024-02-18 21:53:08 +0600

    js some nodes are added

--- JS/script.js
+++ JS/script.js
@@ -6,18 +6,33 @@ let seatArr = [] ;
 for(const seatBtn of seatBtns){
 
     seatBtn.addEventListener('click' , (e) => {
-        seatArr.push(e.target.innerText)
+        
+        if(!seatArr.includes(e.target.innerText) && !seatArr.length <= 4){
+            seatArr.push(e.target.innerText)
+
+            const seatsTotal = getElementValueById('total-seats')
+            if(seatsTotal > 36){
+                const seatsTotal = getElementValueById('total-seats')
+                const decrement = seatsTotal - 1 ;
+                setElementValueById('total-seats' , decrement)
+            }
+        }
+        
+        onerror = () => {
+            const icrement = 40 ;
+            setElementValueById('total-seats' , icrement)
+            seatArr.pop() ;
+        }
+
         if(seatArr.length <= 4){
-            if(seatArr.includes(e.target.innerText)){
-                setBgColorById(e.target.innerText) ;
-                seatBtn
-            }
-            else{
-            }
+            setBgColorById(e.target.innerText) ;
         }
         else{
-            alert('You can select only 4 seats !')
+            // alert('You can select only 4 seats !')
+            seatArr.pop() ;
         }
+        
     })
 
 }
+
--- JS/utility.js
+++ JS/utility.js
@@ -6,3 +6,15 @@ function setBgColorById (elementId) {
     element.classList.add('hover:bg-green-500')
 }
 
+function getElementValueById (elementId) {
+    const element = document.getElementById(elementId) ;
+    const elementValue = element.innerText ;
+    const value = parseInt(elementValue) ;
+    return value ;
+}
+
+function setElementValueById (elementId , value) {
+    const element = document.getElementById(elementId) ;
+    element.innerText = value ;
+}
+
